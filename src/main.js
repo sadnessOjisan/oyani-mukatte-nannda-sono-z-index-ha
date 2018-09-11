184dcf9f6caa9343cd417b6bef6226bb966457b2
john <yuta_ide@r.recruit.co.jp>
2018-09-11 17:36:20 +0900

    [update] init dev env

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../vendor/css/reset.css";
+
+class App extends React.Component {
+  render() {
+    return <p>はろわ</p>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
