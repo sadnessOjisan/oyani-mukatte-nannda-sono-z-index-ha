db76b021dd851c42b3629e710c978d58e4b5ce0b
john <yuta_ide@r.recruit.co.jp>
2018-09-11 17:55:48 +0900

    [add] sample data

---
+++ src/constants/tableData.js
@@ -0,0 +1,112 @@
+const Frameworks = [
+  "React",
+  "Vue",
+  "Angular",
+  "Mithril",
+  "Ember",
+  "Backbone",
+  "Knockout",
+  "Riot",
+  "Inferno"
+];
+
+const students = ["A君", "B君", "C君", "D君", "E君", "F君", "G君", "H君"];
+
+const favorite = {
+  A君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  },
+  B君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  },
+  C君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  },
+  D君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  },
+  E君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  },
+  F君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  },
+  G君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  },
+  H君: {
+    React: "01",
+    Vue: "03",
+    Angular: "02",
+    Mithril: "01",
+    Ember: "01",
+    Backbone: "05",
+    Knockout: "03",
+    Riot: "01",
+    Inferno: "01"
+  }
+};
+
+const favEnum = {
+  "01": "好き",
+  "02": "ちょっと好き",
+  "03": "普通",
+  "04": "嫌い",
+  "05": "大っ嫌い"
+};
